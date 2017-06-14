81b04dcf0b3fad5f49fdb7487549abde2949f1eb
mironov <aleksandar.n.mironov@gmail.com>
2017-06-14 19:58:34 +0300

    Added winner checker in Tic Tac Toe :)

--- Exercises/TicTacToe.java
+++ Exercises/TicTacToe.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 
 public class TicTacToe {
+	public static final char HUMAN_CHAR = 'x';
+	public static final char COMPUTER_CHAR = 'o';
 	
+	// this is our printer
 	public static String print (char [][] board){
 		StringBuilder sb = new StringBuilder();
 		sb.append("  _   _   _  \n");
@@ -13,6 +16,36 @@ public class TicTacToe {
 			sb.append("\n  _   _   _  \n");
 		}		
 		return sb.toString();
+	}
+	
+	// this is checking for winner
+	public static char checkForWinner (char [][] board){
+		char answer = ' ';
+		
+		if ((board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] == COMPUTER_CHAR) ||
+				(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] == COMPUTER_CHAR) ||
+				(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] == COMPUTER_CHAR) ||
+				(board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] == COMPUTER_CHAR) ||
+				(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] == COMPUTER_CHAR) ||
+				(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] == COMPUTER_CHAR) ||
+				(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == COMPUTER_CHAR) ||
+				(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == COMPUTER_CHAR)){
+			answer = COMPUTER_CHAR;
+			return answer;
+		}
+		
+		if ((board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] == HUMAN_CHAR) ||
+				(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] == HUMAN_CHAR) ||
+				(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] == HUMAN_CHAR) ||
+				(board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] == HUMAN_CHAR) ||
+				(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] == HUMAN_CHAR) ||
+				(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] == HUMAN_CHAR) ||
+				(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == HUMAN_CHAR) ||
+				(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] == HUMAN_CHAR)){
+			answer = HUMAN_CHAR;
+			return answer;
+		}
+		return answer;
 	}
 	
 	//this will give as random number in range (from stackoverflow)
@@ -33,20 +66,29 @@ public class TicTacToe {
 		
 		int moveCounter = 0; //game is only 9 moves, will use this for game stop
 		
+		System.out.println(print(board));
+		
 		while(true){
 			// player move
 			do{
 				System.out.println("Enter valid coordinates for your move(x y)");
 				x = sc.nextInt();
 				y = sc.nextInt();
-			}while(x < 0 || 2 < x || y < 0 || 2 < y || board[x][y] != ' ');
+			} while(x < 0 || 2 < x || y < 0 || 2 < y || board[x][y] != ' ');
 			
-			board[x][y] = 'x';
+			board[x][y] = HUMAN_CHAR;
 			moveCounter++;
 			
-			System.out.println(print(board));
+			System.out.println(print(board));			
 			
-			if(moveCounter>=9){
+			if (checkForWinner(board) == HUMAN_CHAR){
+				System.out.println("GAME OVER!!! \nYOY WIN!!!");
+				break;
+			} else if (checkForWinner(board) == COMPUTER_CHAR){
+				System.out.println("GAME OVER!!! \nComputer WIN!!!");
+				break;
+			} else if(moveCounter >= 9){
+				System.out.println("GAME OVER!!! \nNo winner!!!");
 				break;
 			}
 			
@@ -54,22 +96,27 @@ public class TicTacToe {
 			System.out.println("Computer move:");
 			
 			int randomPosition = randomWithRange(0, 8);
-			while(board[randomPosition/3][randomPosition%3] != ' '){
+			while(board[randomPosition / 3][randomPosition % 3] != ' '){
 				randomPosition++;
 				randomPosition = randomPosition % 9; //if we go out of range
 			}
-			board[randomPosition/3][randomPosition%3] = 'o';
+			board[randomPosition/3][randomPosition%3] = COMPUTER_CHAR;
 			
 			moveCounter++;
 			
 			System.out.println(print(board) + "\n");
 			
-			if(moveCounter>=9){
+			if (checkForWinner(board) == HUMAN_CHAR){
+				System.out.println("GAME OVER!!! \nYOY WIN!!!");
+				break;
+			} else if (checkForWinner(board) == COMPUTER_CHAR){
+				System.out.println("GAME OVER!!! \nComputer WIN!!!");
+				break;
+			} else if(moveCounter >= 9){
+				System.out.println("GAME OVER!!! \nNo winner!!!");
 				break;
 			}
 		}
-		
-		System.out.println("GAME OVER!!!");
 		
 		sc.close();
 	}
