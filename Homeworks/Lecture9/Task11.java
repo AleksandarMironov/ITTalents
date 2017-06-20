8d09d7b66439392075a78d61b97213c81495d248
mironov <aleksandar.n.mironov@gmail.com>
2017-06-20 13:55:05 +0300

    First Task from lecture 9

---
+++ Homeworks/Lecture9/Task11.java
@@ -0,0 +1,39 @@
+
+public class Task11 {
+	public static void main(String[] args) {
+		int [] arr = { 1, 2, 3, 4, 5};
+		
+		//print all elements from int arr
+		printArray(arr);
+		
+		//print first x elements from int arr
+		printArrayRecursion(arr, arr.length);
+	}
+	
+	static void printArray (int [] arr){
+		System.out.print("[ ");
+		for (int i = 0; i < arr.length; i++){
+			System.out.print(arr[i]);
+			if (i == arr.length - 1){
+				System.out.print(" ]");
+			} else {
+				System.out.print(", ");
+			}
+		}
+	}
+	
+	//passing arr in recursion is not the best solution but......
+	static void printArrayRecursion (int [] arr, int elements){
+		if(elements - 1 == 0){
+			System.out.print("[ " + arr[elements - 1] + ", ");			
+		} else if (elements == arr.length) {			
+			printArrayRecursion(arr, elements - 1);
+			System.out.print(arr[elements - 1] + " ]");
+			
+		} else {
+			printArrayRecursion(arr, elements - 1);
+			System.out.print(arr[elements - 1] + ", ");
+		}
+		
+	}
+}
